db8742031a7b088ee599d6c25cd923bddd12bedd
i0Ek3 <kno30826@gmail.com>
2021-01-25 20:51:22 +0800

    Add arguments control

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
+import sys
+
 from numpy import random as rd
 import numpy as np
 
@@ -9,7 +11,6 @@ import rich as r
 
 def verficate(level=10, times=100):
     cnt = [0] * level
-    #p = [0] * level
 
     n = 0
     while n < times:
@@ -19,13 +20,24 @@ def verficate(level=10, times=100):
 
     for i in range(level):
         cnt[i] = np.divide(cnt[i], times)
+        
+        ret = "P({0}) = {1}\n"
+        print(ret.format(i, cnt[i]))
     
     return cnt
 
-# TODO: add argv to control arguments
-def main():
-    p = verficate(100, 1000)
+def output():
+    p = verficate()
     for i in range(len(p)):
         r.print(p[i])
 
-main()
+def main(argv):
+    if len(argv) == 1:
+        verficate()
+    elif len(argv) == 2:
+        verficate(int(argv[1]))
+    else:
+        verficate(int(argv[1]), int(argv[2]))
+
+if __name__ == "__main__":
+    main(sys.argv)
